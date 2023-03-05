3702efc81cd6418fe14272b9720cb748a92f9a8e
Nicola Crivellin <nicola.crivellin98@gmail.com>
2023-03-05 17:17:28 +0100

    Add support for number ranges `a-b`
    Example:
    
        git-breeze add 1-3 5 10-14
    
    adds files 1, 2, 3, 5, 10, 11, 12, 13, 14.

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -8,18 +8,43 @@ use std::io;
 
 pub fn replace_number_args(args: Vec<String>, status: &Status) -> io::Result<Vec<String>> {
     let mut result: Vec<String> = Vec::new();
+
+    // Push a status entry if the index exists in the Status structure
+    let try_push = |i, result: &mut Vec<String>| -> io::Result<()> {
+        let entry = status.get(i).ok_or(io::Error::new(
+            io::ErrorKind::InvalidInput,
+            format!("No status entry with number {} exists.", i))
+        )?;
+        result.push(entry.path.clone());
+
+        return Ok(());
+    };
+
     for arg in args {
-        match arg.parse::<usize>() {
-            Ok(i) => match status.get(i).map(|entry| entry.path.clone()) {
-                Some(path) => {
-                    result.push(path);
-                },
-                None => {
+        // Try to parse the argument as a number range
+        let split = arg.split_once('-')
+            .map(|(a, b)| (a.parse::<usize>(), b.parse::<usize>()))
+            .and_then(|(a, b)| Some((a.ok()?, b.ok()?)));
+
+        match split {
+            Some((begin, end)) => {
+                if begin > end {
                     return Err(io::Error::new(io::ErrorKind::InvalidInput,
-                                              format!("No status entry with number {} exists.", i)));
+                                              format!("Invalid range [{}, {}]", begin, end)));
+                }
+                for i in begin .. end + 1 {
+                    try_push(i, &mut result)?;
                 }
             }
-            _ => result.push(arg),
+            None => {
+                // Try to parse the argument as a single number
+                match arg.parse::<usize>() {
+                    Ok(i) => try_push(i, &mut result)?,
+                    // Unable to parse the argument as a number or a range.
+                    // Add it to the result unaltered.
+                    _     => result.push(arg),
+                }
+            }
         }
     }
 
