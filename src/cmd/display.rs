575a9c710472d1d354087177d9c7ace50fd34529
Nicola Crivellin <nicola.crivellin98@gmail.com>
2023-03-16 14:32:51 +0100

    Add header to `git branch`, refactor display code

--- src/cmd/display.rs
+++ src/cmd/display.rs
@@ -2,20 +2,24 @@ use crate::cmd;
 
 use colored::*;
 
+/// Alternating colors to distinguish rows in long lists
+const ROW_COLORS: [Color; 2] = [
+    Color::TrueColor{ r: 140, g: 140, b: 140 },
+    Color::White,
+];
+
 impl std::fmt::Display for cmd::Status {
     /// Print contents of a `cmd::Status` struct
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         let print = |f: &mut std::fmt::Formatter, entry: &cmd::StatusEntry, i: u32| {
             let color = entry.status.to_color();
-            let row_color = match i % 2 == 0 {
-                true  => Color::White,
-                false => Color::TrueColor{ r: 140, g: 140, b: 140 }
-            };
+            let row_color = ROW_COLORS[(i % 2 == 0) as usize];
             writeln!(f,
                 " {}   {} {}",
                 entry.status.to_string().color(color),
                 format!("[{}]", i).color(row_color),
-                entry.path.color(color))
+                entry.path.color(color)
+            )
         };
 
         let mut i = 0;
@@ -54,15 +58,24 @@ impl std::fmt::Display for cmd::Status {
 impl std::fmt::Display for cmd::Branch {
     /// Print contents of a `cmd::Branch` struct
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        const CUR_BRANCH_COLOR: Color = Color::Magenta;
+        const BRANCH_COLOR: Color = Color::Green;
+
+        // Print header
+        writeln!(f,
+             "Current branch: {}",
+             self.branches.get(self.current)
+                .expect("Current branch not found!")
+                .color(CUR_BRANCH_COLOR)
+        )?;
+        writeln!(f, "")?;
+
+        // Print list of branches
         let it = self.branches.iter().zip(self.commits.iter()).enumerate();
         for (i, (branch, _)) in it {
             let prefix       = if i == self.current { " * " } else { "   " };
-            let branch_color = if i == self.current { Color::Magenta } else { Color::Green };
-            let row_color = match i % 2 == 0 {
-                true  => Color::White,
-                false => Color::TrueColor{ r: 140, g: 140, b: 140 }
-            };
-
+            let branch_color = if i == self.current { CUR_BRANCH_COLOR } else { BRANCH_COLOR };
+            let row_color = ROW_COLORS[(i % 2 == 0) as usize];
             writeln!(f,
                 "{}  [{}] {}",
                 prefix,
